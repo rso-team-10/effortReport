7bcf780ecbd5cb4a338ff66b927902a5a979996b
benja8151 <benjamin.smrdelj@gmail.com>
2020-01-16 00:00:40 +0100

    added cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const expressGraphQL = require("express-graphql");
 const bodyParser = require('body-parser');
 const expressPlayground = require('graphql-playground-middleware-express').default;
-
+const cors = require('cors');
 const schema = require('./schema');
 const app = express();
 
@@ -10,6 +10,7 @@ const app = express();
 app.get('/playground', expressPlayground({ endpoint: '/effortReport/v1/graphql' }));
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(
   '/v1/graphql',
   expressGraphQL({
